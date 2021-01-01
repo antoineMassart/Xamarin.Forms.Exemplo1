b0d2866129c499673f2433f6d24d42c69095928b
User <User@DESKTOP-RQF2T9L>
2021-01-01 20:15:26 +0000

    Add project files.

---
+++ Exemplo1/Exemplo1.Android/Splash_Activity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace Exemplo1.Droid
+{
+    [Activity(Label = "Ex 1", Theme = "@style/MainTheme.Splash", Icon = "@mipmap/ic_launcher", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+
+            // Disable activity slide-in animation
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
---
+++ Exemplo1/Exemplo1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Exemplo1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            TextOutput.Text = "Input = "  + TextInput.Text;
+        }
+    }
+}
